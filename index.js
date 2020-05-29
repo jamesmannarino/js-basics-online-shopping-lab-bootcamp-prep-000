146a0a916a71cc04c01a7f22d6e81026a46d4dd0
James Mannarino <jamesmannarino@gmail.com>
2020-05-29 22:20:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function placeOrder(cardNumber) {
    return `Sorry, we don't have a credit card on file for you.`
   }
   else {
-    let total = total()
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+    let totalPrice = total()
+    return `Your total cost is $${totalPrice}, which will be charged to the card ${cardNumber}.`
   } 
 }
