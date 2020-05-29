17a613edefae3b0ef13246fc158d42cb9821c1c5
James Mannarino <jamesmannarino@gmail.com>
2020-05-29 21:42:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,6 +58,6 @@ function placeOrder(cardNumber) {
   }
   else {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-    setCart(0)
+    return setCart(0)
   }
 }
