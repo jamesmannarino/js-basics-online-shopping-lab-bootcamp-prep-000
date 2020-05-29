b2b8303b1bbb7523d406712e05178d26775c4b91
James Mannarino <jamesmannarino@gmail.com>
2020-05-29 21:18:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber !== '') {
    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+   cart = []
   } else if (cardNumber === '') {
       return `Sorry, we don't have a credit card on file for you.`
   } 
