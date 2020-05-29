ea724cd428432da829570abd6f719757077eaed0
James Mannarino <jamesmannarino@gmail.com>
2020-05-29 22:07:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,6 +57,6 @@ function placeOrder(cardNumber) {
    return `Sorry, we don't have a credit card on file for you.`
   }
   else {
-    return setCart(0);
+    setCart(0);
   } return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
 }
