f6c6f20e8401340b2e3d6e5ac178e4ed8334b65f
James Mannarino <jamesmannarino@gmail.com>
2020-05-29 21:05:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,9 +53,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber !== '') {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  if (cardNumber === '') {
+   return `Sorry, we don't have a credit card on file for you.`
   } else {
-    return `Sorry, we don't have a credit card on file for you.`
+     
+     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   } 
 }
